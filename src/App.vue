40c42c6278a6b3eca48cc308c334d138ed3cfc09
Petya-Skripkin <sk.petya02@mail.ru>
2022-05-31 00:14:18 +0600

    Add Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
     <MyTip :calls="calls" />
-    <MyHeader :logo="logo" :basket="basket" :menu="menu" :placeholder="placeholder" />
+    <MyHeader
+      :logo="logo"
+      :basket="basket"
+      :menu="menu"
+      :placeholder="placeholder"
+    />
+    <div class="flex__conteiner">
+      <MySideBar :links="links" />
+      <div class="slide"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import MyTip from "./components/MyTip.vue";
-import MyHeader from "./components/MyHeader.vue";
+import MyTip from "@/components/MyTip.vue";
+import MyHeader from "@/components/MyHeader.vue";
+import MySideBar from "./components/MySideBar.vue";
 export default {
   name: "App",
   components: {
     MyTip,
     MyHeader,
-},
+    MySideBar,
+  },
   data() {
     return {
       calls: ["87056982391", "87077985572"],
@@ -23,7 +34,7 @@ export default {
         link: "#",
         count: 7,
         title: "Корзина",
-        text: "2 102 820₸"
+        text: "2 102 820₸",
       },
       menu: [
         {
@@ -44,6 +55,56 @@ export default {
         },
       ],
       placeholder: "Ищите среди 10 000 товаров...",
+      links: [
+        {
+          key: 1,
+          href: "#",
+          icon: "smartPhone.svg",
+          title: "Смартфоны и гаджеты",
+        },
+        {
+          key: 2,
+          href: "#",
+          icon: "computer.svg",
+          title: "Ноутбуки и компьютеры",
+        },
+        {
+          key: 3,
+          href: "#",
+          icon: "TV.svg",
+          title: "Телевизоры, аудио и видео",
+        },
+        {
+          key: 4,
+          href: "#",
+          icon: "tech.svg",
+          title: "Бытовая техника",
+        },
+        {
+          key: 5,
+          href: "#",
+          icon: "furniture.svg",
+          title: "Мелкобытовая техника",
+        },
+        {
+          key: 6,
+          href: "#",
+          icon: "camera.svg",
+          title: "Фотоаппараты, видеокамеры",
+        },
+        {
+          key: 7,
+          href: "#",
+          icon: "cars.svg",
+          title: "Автотовары и инструменты",
+        },
+        {
+          key: 8,
+          href: "#",
+          icon: "game.svg",
+          title: "Игры и приставки",
+        },
+      ],
     };
   },
 };
@@ -56,5 +117,15 @@ body {
   margin: 0;
   font-family: "Manrope";
   background: #f7f7f7;
+}
+
+.flex__conteiner {
+  display: flex;
+  margin-top: 24px;
+}
+
+.slide {
+  flex: 4;
+  background: blue;
 }
 </style>
---
+++ src/components/MySideBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="sidebar">
+    <a v-for="link in links" :key="link.key" :href="link.href" class="sidebar__link">
+      <img :src="require(`@/assets/icons/${link.icon}`)" alt="" class="logo" />
+      {{ link.title }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    links: Array,
+  },
+};
+</script>
+
+<style scoped>
+.sidebar {
+  flex: 1;
+  padding: 18px 16px 16px 18px;
+  background: #ffffff;
+  border-radius: 8px;
+  margin-right: 32px;
+  padding-bottom: 100px;
+}
+
+.sidebar__link {
+  display: flex;
+  align-items: center;
+  margin: 22px 0;
+  font-family: "Manrope";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 17px;
+  /* identical to box height, or 131% */
+
+  color: #000000;
+  text-decoration: none;
+}
+
+.logo {
+  margin-right: 10px;
+}
+</style>
