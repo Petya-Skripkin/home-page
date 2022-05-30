967a98a8c44a6ee5aaa9e20e64108aa470536b22
Petya-Skripkin <sk.petya02@mail.ru>
2022-05-30 20:57:38 +0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MyTip />
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyTip from './components/MyTip.vue';
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MyTip
+},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url(@/fonts/font.css);
+
+body {
+  font-family: "Manrope";
+  background: #f7f7f7;
 }
 </style>
---
+++ src/components/MyTip.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="tip">
+    <button class="location">
+      <img class="img" src="@/assets/location.png" />
+      Алматы
+    </button>
+    <!-- <select v-model.trim="nowCalled" class="select" @click="selectClick">
+      <img src="@/assets/phone.png" alt="" class="phone">
+      <option class="call" value="Помощь и консультация" disabled>
+        Помощь и консультация
+      </option>
+      <option v-for="call of calls" :key="call" :value="call">
+        {{ call }}
+      </option>
+    </select> -->
+    <div class="select" @click="OnShow">
+      <img src="@/assets/phone.png" alt="" class="phone" />
+      {{ nowCalled }}
+      <div v-if="show" class="select__conteiner">
+        <label
+          class="label"
+          v-for="call in calls"
+          :key="call"
+          :for="call"
+          @click.stop
+        >
+          <input
+            class="radio"
+            type="radio"
+            name="number"
+            :id="call"
+            :value="call"
+            v-model="nowCalled"
+          />
+          {{ call }}
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      nowCalled: "Помощь и консультация",
+      calls: ["87056982391", "87077985572"],
+    };
+  },
+  methods: {
+    OnShow() {
+      this.show = !this.show;
+    },
+  },
+  watch: {
+    nowCalled() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tip {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.location {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 7.5px 13px;
+
+  background: #ffffff;
+  /* Evrika/gray */
+
+  border: 1px solid #e0e0e0;
+  border-radius: 30px;
+  cursor: pointer;
+}
+
+.select {
+  display: flex;
+  position: relative;
+  min-width: 200px;
+  padding: 8px;
+  padding-right: 30px;
+  padding-left: 40px;
+  background: #ffffff;
+  /* Evrika/gray */
+
+  border: 1px solid #e0e0e0;
+  border-radius: 30px;
+
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  outline: 0;
+  background-image: url("@/assets/ArrowChevronDown.png");
+  background-repeat: no-repeat;
+  background-position: 95% 55%;
+  cursor: pointer;
+}
+
+.select__conteiner {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 30px;
+  left: 0;
+  width: 100%;
+
+  background: #ffffff;
+  /* Evrika/gray */
+
+  border: 1px solid #e0e0e0;
+  border-radius: 2px 2px 10px 10px;
+  cursor: pointer;
+}
+
+.radio {
+  display: none;
+}
+
+.label {
+  padding: 5px 15px;
+  text-align: center;
+  cursor: pointer;
+
+  border-radius: 2px 2px 10px 10px;
+}
+
+.label:hover {
+  background: #e0e0e0;
+}
+
+.phone {
+  height: 24px;
+  position: absolute;
+  left: 13px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+}
+</style>
